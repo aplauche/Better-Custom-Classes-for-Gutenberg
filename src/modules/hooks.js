22c036e3f2091cd1b782d6a1033e3ac6cef6c190
Anton Plauche <antonplauche92@gmail.com>
2022-09-17 11:51:07 -0500

    add in use copy hook for future versions. sticking with copybutton for now

---
+++ src/modules/hooks.js
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+
+export const useCopyToClipboard = () => {
+  const [copiedText, setCopiedText] = useState(null)
+
+  const copy = async text => {
+    if (!navigator?.clipboard) {
+      alert(`Clipboard not supported - you can manually copy here: ${text}`)
+      return false
+    }
+
+    // Try to save to clipboard then save it in the state if worked
+    try {
+      await navigator.clipboard.writeText(text)
+      setCopiedText(text)
+      return true
+    } catch (error) {
+      console.warn('Copy failed', error)
+      setCopiedText(null)
+      return false
+    }
+  }
+
+  return [copiedText, copy]
+}
+
+
