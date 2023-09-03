af1bd24a2c761c75e8660e423bcaa51aa546f6a2
Anton Plauche <antonplauche92@gmail.com>
2023-09-03 11:32:33 -0500

    clean out main php file

--- better-custom-classes-for-gutenberg.php
+++ better-custom-classes-for-gutenberg.php
@@ -40,8 +40,6 @@ class BetterCustomClassesForGutenberg
     add_action('admin_init', array($this, 'add_settings'));
     add_action('rest_api_init', array($this, 'add_settings'));
 
-    // add_action('admin_post_nopriv_bccfg_class_library_form', array($this, 'handle_class_library_submit'));
-    // add_action('admin_post_bccfg_class_library_form', array($this, 'handle_class_library_submit'));
   }
 
   // Utility for sanitizing an array of class attributes
@@ -128,102 +126,6 @@ class BetterCustomClassesForGutenberg
       )
     );
     register_setting('bccfg-class-library-settings', 'bccfg_class_library', $args);
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-  function library_save_callback()
-  {
-
-    $classList = get_option('bccfg_class_library');
-
-    if (!is_array($classList)) {
-      $classList = [];
-    }
-
-    ?>
-        <div class="wrap">
-          <div id="icon-tools" class="icon32"></div>
-          <h2>Gutenberg Class Library</h2>
-          <?php
-
-          // Check if status is 1 which means a successful options save just happened
-          if (isset($_GET['status']) && $_GET['status'] == 1) : ?>
-
-            <div class="notice notice-success inline">
-              <p>Options Saved!</p>
-            </div>
-
-          <?php endif;
-
-          ?>
-          <form action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="POST">
-
-            <h3>Your remembered classes:</h3>
-            <p>You can enter a list of comma-seperated classes here to populate your autocomplete class library.</p>
-
-            <!-- The nonce field is a security feature to avoid submissions from outside WP admin -->
-            <?php wp_nonce_field('bccfg_options_verify', 'bccfg_update_library'); ?>
-
-            <div>
-              <textarea name="class-list" rows="8" cols="50"><?php echo $classList ? esc_html(trim(implode(',', $classList))) : ''; ?></textarea>
-            </div>
-            <input type="hidden" name="action" value="bccfg_class_library_form">
-            <input type="submit" name="submit" id="submit" class="update-button button button-primary" value="Update Classes" />
-          </form>
-        </div>
-    <?php }
-
-  function handle_class_library_submit()
-  {
-    // Make sure user actually has the capability to edit the options
-    if (!current_user_can('edit_theme_options')) {
-      wp_die("You do not have permission to view this page.");
-    }
-
-    // pass in the nonce ID from our form's nonce field - if the nonce fails this will kill script
-    if (check_admin_referer('bccfg_options_verify', 'bccfg_update_library')) {
-
-      if (isset($_POST['class-list'])) {
-
-        preg_replace('/\W+/', '', strtolower(strip_tags($item)));
-
-        // trim and sanitize
-        $classListString = trim(sanitize_text_field($_POST['class-list']));
-
-        // convert to an array of the classes
-        $classList = explode(',', $classListString);
-
-        // Map over array and validate that each is a valid class name attribute
-        // Not really needed since we have sanitize callback, but doesn't hurt either
-        $classList = $this->sanitize_array_of_classes($classList);
-
-        // Add or update the option
-        $option_exists = get_option('bccfg_class_library');
-
-        if (!empty($classList) && !empty($option_exists)) {
-          update_option('bccfg_class_library', $classList);
-        } else {
-          add_option('bccfg_class_library', $classList);
-        }
-      }
-      wp_redirect($_SERVER['HTTP_REFERER'] . '&status=1');
-    } else {
-      wp_die("You do not have permission to view this page.");
-    }
   }
 }
 
